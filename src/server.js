206868cf6b05de49faaee893ce3338238e355e42
DongDucLan <landd.nde19036@vtc.edu.vn>
2022-04-20 12:43:05 +0700

    Init project

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import mapOrder from './utils/sorts.js'
+
+const app = express()
+const hostname = 'localhost'
+const port = 8017
+
+app.get('/', (req, res) => {
+    res.end('<h1>Hello World</h1><hr/>')
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Hello kassdev, I'm runnung at ${hostname}:${port}/`)
+})
